5c8a24c43412cda45a67b39b9b3d19cd19bcc546
hyeri6318 <lhl6318@naver.com>
2024-09-23 20:52:46 +0900

    domain 패키지_연관관계 없는 Entity 개발

--- src/main/kotlin/com/hyeri/portfolio/domain/entity/BaseEntity.kt
+++ src/main/kotlin/com/hyeri/portfolio/domain/entity/BaseEntity.kt
@@ -1,6 +1,20 @@
 package com.hyeri.portfolio.domain.entity
 
+import jakarta.persistence.Column
 import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.LocalDateTime
 
 @MappedSuperclass
-abstract class BaseEntity
+abstract class BaseEntity {
+
+    @CreatedDate    // 생성 일시 (JPA Entity가 생성된 시간을 자동으로 세팅)
+    @Column(nullable = false, updatable = false)
+    // NULL 안됨, UPDATE 불가능 (이유 : Entity는 생성되고 나서 생성 일시가 수정되면 안됨.)
+    var createdDateTime: LocalDateTime = LocalDateTime.now()
+
+    @LastModifiedDate   // 마지막으로 수정된 일시
+    @Column(nullable = false)
+    var updatedDateTime: LocalDateTime = LocalDateTime.now()
+}
--- src/main/kotlin/com/hyeri/portfolio/domain/entity/Skill.kt
+++ src/main/kotlin/com/hyeri/portfolio/domain/entity/Skill.kt
@@ -1,13 +1,22 @@
 package com.hyeri.portfolio.domain.entity
 
+import com.hyeri.portfolio.domain.constant.SkillType
 import jakarta.persistence.*
 
 @Entity
-class Skill : BaseEntity() {
+class Skill(
+    var name: String,
+    type: String,
+    var isActive: Boolean
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "skill_id")
     var id: Long? = null
+
+    @Column(name = "skill_type")
+    @Enumerated(value = EnumType.STRING)
+    var type: SkillType = SkillType.valueOf(type)
 
 }
