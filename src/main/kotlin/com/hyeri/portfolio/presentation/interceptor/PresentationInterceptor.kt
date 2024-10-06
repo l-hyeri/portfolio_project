c3209361ec27d637a14239db96ddf91e40cf2b03
hyeri6318 <lhl6318@naver.com>
2024-10-06 22:23:10 +0900

    인터셉터 개발

--- src/main/kotlin/com/hyeri/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/hyeri/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,7 +1,24 @@
 package com.hyeri.portfolio.presentation.interceptor
 
+import com.hyeri.portfolio.domain.entity.HttpInterface
+import com.hyeri.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import java.lang.Exception
 
 @Component  // 스프링 빈 등록
-class PresentationInterceptor {
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface=HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
 }
--- src/main/kotlin/com/hyeri/portfolio/presentation/interceptor/PresentationInterceptorConfiguration.kt
+++ src/main/kotlin/com/hyeri/portfolio/presentation/interceptor/PresentationInterceptorConfiguration.kt
@@ -1,7 +1,20 @@
 package com.hyeri.portfolio.presentation.interceptor
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class PresentationInterceptorConfiguration {
+class PresentationInterceptorConfiguration(
+    private val presentationInterceptor: PresentationInterceptor
+) : WebMvcConfigurer {
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(presentationInterceptor)
+            .addPathPatterns("/**")
+            .excludePathPatterns(
+                "/assets/**", "/css/**", "/admin/**", "h2**",
+                "/favicon.ico", "/error"
+            )
+    }
 }
