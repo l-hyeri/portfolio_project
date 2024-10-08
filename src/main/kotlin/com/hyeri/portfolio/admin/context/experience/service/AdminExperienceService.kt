2bb27a460fe7128c6609eea7bb132064c86e6d40
hyeri6318 <lhl6318@naver.com>
2024-10-08 13:37:28 +0900

    Admin 패키지_연관관계 있는 Entity 개발(조회)

---
+++ src/main/kotlin/com/hyeri/portfolio/admin/context/experience/service/AdminExperienceService.kt
@@ -0,0 +1,32 @@
+package com.hyeri.portfolio.admin.context.experience.service
+
+import com.hyeri.portfolio.admin.data.TableDTO
+import com.hyeri.portfolio.admin.exception.AdminBadRequestException
+import com.hyeri.portfolio.domain.entity.Experience
+import com.hyeri.portfolio.domain.entity.ExperienceDetail
+import com.hyeri.portfolio.domain.repository.ExperienceRepository
+import org.springframework.stereotype.Service
+
+@Service
+class AdminExperienceService(   // experience 테이브 조회하기, experience detail 테이블 조회하기 2가지 기능 가짐ㄴ
+    private val experienceRepository: ExperienceRepository
+) {
+
+    fun getExperienceTable(): TableDTO {
+        val classInfo = Experience::class
+        val entities = experienceRepository.findAll()
+
+        return TableDTO.from(classInfo, entities,"details")
+    }
+
+    fun getExperienceDetailTable(id: Long?): TableDTO {   // 상세 조회
+        val classInfo = ExperienceDetail::class
+        val entities = if (id != null) experienceRepository.findById(id)
+            .orElseThrow {
+                throw AdminBadRequestException("ID ${id}에 해당하는 데이터를 찾을 수가 없습니다.")
+            }
+            .details else emptyList()
+
+        return TableDTO.from(classInfo, entities)
+    }
+}
