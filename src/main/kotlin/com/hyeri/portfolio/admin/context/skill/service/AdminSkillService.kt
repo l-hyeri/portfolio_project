6f5ec008ef394ad18bfd205a535d4373c444caee
hyeri6318 <lhl6318@naver.com>
2024-10-08 09:13:28 +0900

    Admin 패키지_연관관계 없는 Entity 개발

---
+++ src/main/kotlin/com/hyeri/portfolio/admin/context/skill/service/AdminSkillService.kt
@@ -0,0 +1,18 @@
+package com.hyeri.portfolio.admin.context.skill.service
+
+import com.hyeri.portfolio.admin.data.TableDTO
+import com.hyeri.portfolio.domain.entity.Skill
+import com.hyeri.portfolio.domain.repository.SkillRepository
+import org.springframework.stereotype.Service
+
+@Service
+class AdminSkillService(
+    private val skillRepository: SkillRepository
+) {
+    fun getSkillTable(): TableDTO {
+        val classInfo = Skill::class
+        val entities = skillRepository.findAll()
+
+        return TableDTO.from(classInfo, entities)
+    }
+}
